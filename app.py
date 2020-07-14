5f5b1c36d6d5a8b40554cccf84702f5437016d80
shivenk78 <kumar.shiven15@gmail.com>
2020-07-14 13:15:11 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        pass
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        post = request.form.get('post')
+        create_post(name, post)
+
+    posts = get_posts()
+
+    return render_template('index.html', posts=posts)
